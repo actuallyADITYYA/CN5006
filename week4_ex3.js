f881d5f8126c8e0d27c88f4a78f9909324d050a8
adityaprakash18913 <adityaprakash18913@gmail.com>
2024-10-23 14:37:00 +0100

    final

--- week4_ex3.js
+++ week4_ex3.js
@@ -34,3 +34,41 @@ app.get('/GetStudents',function (req,res)
 data)});
 });
 })
+
+app.get('/GetStudentid/:id',(req,res)=>{
+    studentdata={}
+    fs.readFile(__dirname + "/" + "Student.json", 'utf8', 
+    function (err, data) {
+   
+    var students= JSON.parse(data)
+    var student=students["Student"+req.params.id]
+    console.log("student",student)
+    if (student)
+   
+    res.json(student)
+    else
+    res.json({ 'status':true, 'Status_Code':200,
+    'requested at': req.localtime, 'requrl':req.url,
+    'request Method':req.method, 'studentdata':JSON.parse(data)});
+    });
+   
+   
+})
+
+app.get('/studentinfo',function(req,res)
+{
+    res.sendFile('StudentInfo.html', { root: __dirname });
+})
+app.post('/submit-data', function (req, res) {
+    var name = req.body.firstName + ' ' + req.body.lastName+
+    ' ';
+    var Age= req.body.myAge+ ' Gender: ' + req.body.gender+' '
+    Qual= ' Qualification'+ req.body.Qual
+    console.log(req.body.Qual)
+    res.send({
+    status: true,
+    message: 'form Details', data: {
+    name: name, age:Age, Qualification:Qual,
+    }
+  });
+});
