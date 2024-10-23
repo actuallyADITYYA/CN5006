e3d7945c3042810fc1a5167c882cfa3a384b7677
adityaprakash18913 <adityaprakash18913@gmail.com>
2024-10-23 13:37:36 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express=require("express")
+var fs= require("fs")
+var app=express()
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/',function(req,res){
+res.send("hello it is my first express application")
+})
+
+app.listen(3000,function(){
+    console.log("server is running on port 3000")
+})
